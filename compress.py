40595469dbb6c2f4c3d433e68b5a028e700081b9
Greg Da Silva <68104918+GregDaSilva@users.noreply.github.com>
2021-10-03 17:55:38 +0200

    Updated algorithm to work with any file

--- compress.py
+++ compress.py
@@ -4,27 +4,33 @@ f = raw_input("\n Hello, user. "
     "\n \n Please type in the path to your file and press 'Enter': ")
 file = open(f, 'r')
 
-os.system("tar czf " + f + '.tar.gz' + " " + f)
+os.system("mv " + f + " rawData.csv")
+os.system("tar czf rawData.csv.tar.gz rawData.csv")
 
 #print("File size for original file is: ")
-os.system("ls -l " + f + "> originalsize.txt")
+os.system("ls -l rawData.csv > originalsize.txt")
 
 #print("File size for compressed file is: ")
-os.system("ls -l " + f + '.tar.gz' + "> compressedsize.txt")
+os.system("ls -l rawData.csv.tar.gz > compressedsize.txt")
 
 
 
 with open('originalsize.txt') as a:
     line = a.readline()  # read up to 8 chars from first line
-print("Size of original file is " + line[19:-48] + " Bytes")
+print("Size of original file is " + line[19:-25] + " Bytes")
 
 with open('compressedsize.txt') as a:
     compressedline = a.readline()  # read up to 8 chars from first line
-print("Size of compressed file is " + compressedline[19:-55] + " Bytes")
+print("Size of compressed file is " + compressedline[19:-32] + " Bytes")
 
-originalsize = float(line[19:-48])
-compressedsize = float(compressedline[19:-55])
+originalsize = float(line[19:-25])
+compressedsize = float(compressedline[19:-32])
 
 reduction = ((originalsize-compressedsize)/originalsize)*100
 
 print("Compression ratio : " + str(reduction) + "%")
+
+
+os.system("mv rawData.csv " + f)
+os.system("mv rawData.csv.tar.gz " + f + ".tar.gz")
+

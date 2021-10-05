e4633ca1ffe57466c3abf3ba4d8f574353ea0ad6
Greg Da Silva <68104918+GregDaSilva@users.noreply.github.com>
2021-10-05 16:13:26 +0200

    Update DesignDecrypDecomp.py

--- DesignDecrypDecomp.py
+++ DesignDecrypDecomp.py
@@ -1,8 +1,12 @@
+import os
 
 
 
 
 
+f = raw_input("\n Hello, user. "
+    "\n \n Please type in the path to your file and press 'Enter': ")
+file = open(f, 'r')
 
 
 os.system("mv " + f + " original")

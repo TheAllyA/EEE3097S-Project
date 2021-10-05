32df81f48ebee6fa37ab3da6c64174eb85951352
TheAllyA <88887387+TheAllyA@users.noreply.github.com>
2021-10-05 22:00:24 +0200

    Updated code in file

--- DesignDecrypDecomp.py
+++ DesignDecrypDecomp.py
@@ -1,12 +1,23 @@
 import os
+from cryptography.fernet import Fernet
+
+with open('finalEncrypt.csv.tar.gz','rb') as e:  
+    encrypted = e.read()
+
+decrypted_file = f.decrypt(encrypted)
+
+with open('finalDecrypt.csv.tar.gz','wb') as finalDecrypt:
+    finalDecrypt.write(decrypted_file)
+
+#f = raw_input("\n Hello, user. "
+#    "\n \n Please type in the path to your file and press 'Enter': ")
+file = open(finalDecrypt, 'r')
 
 
+os.system("mv " + finalDecrypt + " original")
+os.system("tar xzf " + finalDecrypt + ".tar.gz")
+os.system("mv rawData.csv decompressed")
+string = os.system("cmp original decompressed")
 
-
-
-f = raw_input("\n Hello, user. "
-    "\n \n Please type in the path to your file and press 'Enter': ")
-file = open(f, 'r')
-
-
-os.system("tar xzf " + f)
+if string == 0 :
+        print("No difference between files")

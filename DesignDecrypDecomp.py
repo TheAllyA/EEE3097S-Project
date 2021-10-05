8e63192653dd59802d0d78f80abca01aa74885f4
Greg Da Silva <68104918+GregDaSilva@users.noreply.github.com>
2021-10-05 16:12:19 +0200

    Create DesignDecrypDecomp.py

--- DesignDecrypDecomp.py
+++ DesignDecrypDecomp.py
@@ -1,1 +1,14 @@
 
+
+
+
+
+
+
+os.system("mv " + f + " original")
+os.system("tar xzf " + f + ".tar.gz")
+os.system("mv rawData.csv decompressed")
+string = os.system("cmp original decompressed")
+
+if string == 0 :
+        print("No difference between files")
